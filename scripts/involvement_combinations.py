637a68e837b1e11a38cffd173a15030b2887c655
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2023-07-25 15:27:10 +0200

    add scripts for tables 1, 2, and 3 in manuscript

---
+++ scripts/involvement_combinations.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Simultaneous involvement in levels I, II, and III for the whole patient cohort (all),
+stratified according to early (T1/T2) versus advanced (T3/T4) T-category, and
+stratified according to ECE. Columns 1-3 define the 8 possible combinations of
+involvement; subsequent columns report the number of patients with the respective
+combination of co-involved levels.
+"""
+# pylint: disable=import-error
+# pylint: disable=singleton-comparison
+from itertools import product
+from pathlib import Path
+import warnings
+
+import pandas as pd
+from shared import DATAFILE, TABLES_DIR, load_and_prepare_data
+
+warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
+
+OUTPUT_NAME = Path(__file__).with_suffix(".csv").name
+LNLS = ["I", "II", "III", "IV", "V"]
+
+
+if __name__ == "__main__":
+    dataset, max_llh_data = load_and_prepare_data(filepath=DATAFILE, lnls=LNLS)
+
+    is_nplus = max_llh_data.sum(axis=1) != 0
+    has_extracapsular = dataset["patient", "#", "extracapsular"] == True
+    has_no_extracapsular = dataset["patient", "#", "extracapsular"] == False
+    has_contra_involvement = max_llh_data["contra"].sum(axis=1) != 0
+    column_conditions = {
+        ("ipsi"  , "all"  ): [True] * len(dataset),
+        ("ipsi"  , "T1/T2"): dataset["tumor", "1", "t_stage"].isin([1, 2]),
+        ("ipsi"  , "T3/T4"): dataset["tumor", "1", "t_stage"].isin([3, 4]),
+        ("ipsi"  , "ECE+" ): has_extracapsular & is_nplus,
+        ("ipsi"  , "ECE-" ): has_no_extracapsular & is_nplus,
+        ("contra", "all"  ): [True] * len(dataset),
+    }
+
+    tf2pn = lambda tf: "pos" if tf else "neg"
+
+    columns = pd.MultiIndex.from_tuples(column_conditions.keys())
+    index = pd.MultiIndex.from_tuples(
+        product(["pos", "neg"], repeat=3),
+        names=["I", "II", "III"],
+    )
+    table = pd.DataFrame(index=index, columns=columns)
+
+    for (side, col), col_condition in column_conditions.items():
+        row_conditions = {}
+        for lnl_I, lnl_II, lnl_III in product([True, False], repeat=3):
+            row_conditions[(tf2pn(lnl_I), tf2pn(lnl_II), tf2pn(lnl_III))] = (
+                (max_llh_data[side]["I"] == lnl_I)
+                & (max_llh_data[side]["II"] == lnl_II)
+                & (max_llh_data[side]["III"] == lnl_III)
+            )
+        for row, row_condition in row_conditions.items():
+            subset = max_llh_data[side].loc[col_condition & row_condition]
+            table.loc[row, (side, col)] = len(subset)
+
+    table.to_csv(TABLES_DIR / OUTPUT_NAME)
--- scripts/prevalence_by_subsite.py
+++ scripts/prevalence_by_subsite.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Create a table with per-level involvement prevalence for patients with and without
-extracapsular extension (ECE).
+Ipsilateral and contralateral involvement stratified according to primary tumor subsite.
 """
 # pylint: disable=import-error
 # pylint: disable=singleton-comparison
@@ -12,27 +11,32 @@ from shared import DATAFILE, TABLES_DIR, load_and_prepare_data
 
 OUTPUT_NAME = Path(__file__).with_suffix(".csv").name
 LNLS = ["I", "II", "III", "IV", "V"]
+SUBSITES = {
+    "tongue": ["C02", "C02.0", "C02.1", "C02.2", "C02.3", "C02.4", "C02.8", "C02.9"],
+    "gums & cheek": ["C03", "C03.0", "C03.1", "C03.9", "C06", "C06.0", "C06.1", "C06.2", "C06.8", "C06.9"],
+    "floor of mouth": ["C04", "C04.0", "C04.1", "C04.8", "C04.9"],
+}
 
 if __name__ == "__main__":
     dataset, max_llh_data = load_and_prepare_data(filepath=DATAFILE, lnls=LNLS)
 
     is_nplus = max_llh_data.sum(axis=1) != 0
     subset_condition = {
-        "N+": True,
-        "ECE+": dataset["patient", "#", "extracapsular"] == True,
-        "ECE?": dataset["patient", "#", "extracapsular"].isna(),
-        "ECE-": dataset["patient", "#", "extracapsular"] == False,
+        "all": [True] * len(dataset),
+        "tongue": dataset["tumor", "1", "subsite"].isin(SUBSITES["tongue"]),
+        "gums & cheek": dataset["tumor", "1", "subsite"].isin(SUBSITES["gums & cheek"]),
+        "floor of mouth": dataset["tumor", "1", "subsite"].isin(SUBSITES["floor of mouth"]),
     }
 
     columns = ["total", *LNLS]
     index = pd.MultiIndex.from_product(
-        [["ipsi", "contra"], ["N+", "ECE+", "ECE?", "ECE-"]]
+        [["ipsi", "contra"], list(subset_condition.keys())]
     )
     table = pd.DataFrame(index=index, columns=columns)
 
     for side in ["ipsi", "contra"]:
-        for row in ["N+", "ECE+", "ECE?", "ECE-"]:
-            subset = max_llh_data[side].loc[is_nplus & subset_condition[row]]
+        for row, condition in subset_condition.items():
+            subset = max_llh_data[side].loc[condition]
             table.loc[(side, row), "total"] = len(subset)
 
             for lnl in LNLS:
